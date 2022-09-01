83b7ff70f2a3c9b8606a6166dea64c8e0a4d395c
Vishv Deshwal <vishavdeshwal@gmail.com>
2022-09-01 13:00:09 +0530

    Added a comment...

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 4;
         int b = 2;
+        // You are advised to follow certain things, and be cautious to debug code every time you face an error
 
         System.out.println(a+b);
 
